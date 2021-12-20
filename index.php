63cf7fe7894cc4e38a0911722e89574fae2f0320
Lionel Moisan <lmoisan@digitalim.fr>
2021-12-20 12:08:11 +0100

    ajout + modification

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php 
+<?php
+
+// Affiche toutes les informations, comme le ferait INFO_ALL
+phpinfo();
+
+// Affiche uniquement le module d'information.
+// phpinfo(8) fournirait les mêmes informations.
+phpinfo(INFO_MODULES);
 
 ?>
